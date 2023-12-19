01708f15e3b15e956319e2896beb5cdd92d45feb
Ashwin krishna U <95433523+Ashkrish-25@users.noreply.github.com>
2023-12-19 23:17:38 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import WeatherApp from './Components/WeatherApp/WeatherApp';
+import './App.css';
+
+const App = () =>{
+    return (
+        <div className='App'>
+            <WeatherApp />
+        </div>
+    );
+}
+
+export default App; 
